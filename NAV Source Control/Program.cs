9f324759cde4288a9af5d93abfa0e1307d3543bb
Torben Nehmer <torben.nehmer@terrabit.de>
2016-11-19 22:35:33 +0100

    Added basic log4net config file

--- NAV Source Control/Program.cs
+++ NAV Source Control/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
-namespace NAV_Source_Control
+namespace NAVSCM
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo("Config Files\\log4net.config"));
+
+            log.Info("Starting up...");
+
+            log.Debug("Debug");
+            log.Warn("Warn");
+            log.Error("Error");
+            log.Fatal("Fatal");
+
+            log.Info("Shutting down...");
+
+            Console.ReadLine();
         }
     }
 }
