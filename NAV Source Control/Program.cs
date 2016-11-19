e3b3b9dd858ab8f9cd8dff97fe442aa63d51ae2a
Torben Nehmer <torben.nehmer@terrabit.de>
2016-11-19 22:00:13 +0100

    Initial Project Setup

---
+++ NAV Source Control/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAV_Source_Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
